fbfac30b4e6735f7920e0424b3f005161678ae4f
wangxiao <986542500@qq.com>
2023-11-22 19:01:47 +0800

    基于redis实现分布式锁
    Signed-off-by: wangxiao <986542500@qq.com>

--- hm-dianping/src/main/java/com/hmdp/utils/SimpleRedisLock.java
+++ hm-dianping/src/main/java/com/hmdp/utils/SimpleRedisLock.java
@@ -1,16 +1,29 @@
 package com.hmdp.utils;
 
 import ch.qos.logback.core.util.TimeUtil;
+import cn.hutool.core.lang.UUID;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.script.DefaultRedisScript;
 
 import javax.annotation.Resource;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.concurrent.TimeUnit;
 
 public class SimpleRedisLock implements ILock{
     private static final  String KEY_PREFIX = "lock:";
+    private static final  String ID_PREFIX = UUID.randomUUID().toString(true)+"-";
+    private static final DefaultRedisScript<Long> UNLOCK_SCRIPT;
+    static {
+        UNLOCK_SCRIPT = new DefaultRedisScript<>();
+        UNLOCK_SCRIPT.setLocation(new ClassPathResource("unlock.lua"));
+        UNLOCK_SCRIPT.setResultType(Long.class);
+    }
     //业务名字，也是将来锁的名字
     private String name;
     private StringRedisTemplate stringRedisTemplate;
+
 
     public SimpleRedisLock(String name, StringRedisTemplate stringRedisTemplate) {
         this.name = name;
@@ -20,10 +33,10 @@ public class SimpleRedisLock implements ILock{
     @Override
     public boolean tryLock(long timeoutSec) {
         //获取线程标识
-        long threadId = Thread.currentThread().getId();
+        String threadId = ID_PREFIX + Thread.currentThread().getId();
         // 获取锁
         Boolean success = stringRedisTemplate.opsForValue()
-                .setIfAbsent(KEY_PREFIX + name, threadId + "",
+                .setIfAbsent(KEY_PREFIX + name, threadId,
                         timeoutSec, TimeUnit.SECONDS);
         //有自动拆箱就有安全风险
         return Boolean.TRUE.equals(success);
@@ -31,6 +44,18 @@ public class SimpleRedisLock implements ILock{
 
     @Override
     public void unlock() {
-        stringRedisTemplate.delete(KEY_PREFIX + name);
+        //调用lua脚本
+        stringRedisTemplate.execute(
+                UNLOCK_SCRIPT,
+                Collections.singletonList(KEY_PREFIX+name),
+                        ID_PREFIX+Thread.currentThread().getId());
+//        // 获取线程标识
+//        String threadId = ID_PREFIX + Thread.currentThread().getId();
+//        //判断标示是否一致
+//        String id = stringRedisTemplate.opsForValue().get(KEY_PREFIX + name);
+//        if(threadId.equals(id)){
+//            //释放锁
+//            stringRedisTemplate.delete(KEY_PREFIX + name);
+//        }
     }
 }
